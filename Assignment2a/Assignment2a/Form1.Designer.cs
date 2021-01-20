25d6a9f98844570d446fbdb79ff6e00ba3833a61
kimkev <43860707+kimkev@users.noreply.github.com>
2021-01-20 14:02:29 -0500

    Add files via upload

---
+++ Assignment2a/Assignment2a/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Assignment2a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAuthor = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblISBN = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.lstBook = new System.Windows.Forms.ListBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(130, 417);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(236, 112);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(422, 121);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(308, 31);
+            this.txtName.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(125, 121);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(148, 25);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Name (50 chr)";
+            // 
+            // lblAuthor
+            // 
+            this.lblAuthor.AutoSize = true;
+            this.lblAuthor.Location = new System.Drawing.Point(125, 192);
+            this.lblAuthor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAuthor.Name = "lblAuthor";
+            this.lblAuthor.Size = new System.Drawing.Size(155, 25);
+            this.lblAuthor.TabIndex = 5;
+            this.lblAuthor.Text = "Author (50 chr)";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(125, 263);
+            this.lblDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(134, 25);
+            this.lblDate.TabIndex = 6;
+            this.lblDate.Text = "Publish Date";
+            // 
+            // lblISBN
+            // 
+            this.lblISBN.AutoSize = true;
+            this.lblISBN.Location = new System.Drawing.Point(127, 331);
+            this.lblISBN.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblISBN.Name = "lblISBN";
+            this.lblISBN.Size = new System.Drawing.Size(66, 25);
+            this.lblISBN.TabIndex = 7;
+            this.lblISBN.Text = "ISBN ";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(448, 60);
+            this.lblId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(183, 25);
+            this.lblId.TabIndex = 8;
+            this.lblId.Text = "ID auto generated";
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(422, 263);
+            this.txtDate.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(308, 31);
+            this.txtDate.TabIndex = 10;
+            this.txtDate.Text = "YYYY-MM-DD";
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(422, 331);
+            this.txtISBN.Margin = new System.Windows.Forms.Padding(4);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(308, 31);
+            this.txtISBN.TabIndex = 11;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(130, 588);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(236, 112);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(443, 588);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(236, 112);
+            this.btnEdit.TabIndex = 13;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(443, 417);
+            this.btnView.Margin = new System.Windows.Forms.Padding(4);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(236, 112);
+            this.btnView.TabIndex = 14;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // lstBook
+            // 
+            this.lstBook.FormattingEnabled = true;
+            this.lstBook.HorizontalScrollbar = true;
+            this.lstBook.ItemHeight = 25;
+            this.lstBook.Location = new System.Drawing.Point(759, 60);
+            this.lstBook.Margin = new System.Windows.Forms.Padding(4);
+            this.lstBook.Name = "lstBook";
+            this.lstBook.Size = new System.Drawing.Size(494, 654);
+            this.lstBook.TabIndex = 15;
+            this.lstBook.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(422, 192);
+            this.txtAuthor.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(308, 31);
+            this.txtAuthor.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1389, 769);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.lstBook);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.lblISBN);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblAuthor);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnAdd);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(1594, 867);
+            this.MinimumSize = new System.Drawing.Size(1000, 500);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblISBN;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.ListBox lstBook;
+        private System.Windows.Forms.TextBox txtAuthor;
+    }
+}
+
---
+++ Assignment2a/Assignment2a/Form1.cs
@@ -0,0 +1,190 @@
+﻿/* Assignment 2 Task 1: Books form
+ * Kevin Kim 
+ * 991 456 570
+ * June 13, 2018
+ */
+using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Assignment2a
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = null;
+        SqlConnection cnn;
+        SqlCommand command;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Books";
+            connectionString = "Data Source=DESKTOP-4P2PB1M\\SQLEXPRESS;" +
+                              "Initial Catalog = MegaBookDB;" +
+                              "Integrated Security = SSPI; " +
+                              "Persist Security Info = false";
+
+            cnn = new SqlConnection(connectionString);
+        }
+
+        // event handler for add button
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cnn.Open();
+                command = new SqlCommand("Insert into dbo.BOOKS values(@bookname, @author," +
+                                         "@publishdate, @ISBN)", cnn);
+
+                command.Parameters.AddWithValue("@bookname", this.txtName.Text);
+                command.Parameters.AddWithValue("@author", this.txtAuthor.Text);
+                command.Parameters.AddWithValue("@publishdate", this.txtDate.Text);
+                command.Parameters.AddWithValue("@ISBN", this.txtISBN.Text);
+
+                int r = command.ExecuteNonQuery();
+
+                MessageBox.Show(r + " Book record added");
+
+                cnn.Close();
+            }
+
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error in SQL! " + ex.Message);
+            }
+
+            finally
+            {
+                if (cnn.State == ConnectionState.Open)
+                {
+                    cnn.Close();
+                }
+            }
+        }
+
+        // event handler for view button
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            string queryString = "select * from dbo.BOOKS";
+
+            try
+            {
+
+                // empty out the listbox first
+                this.lstBook.Items.Clear();
+
+                cnn.Open();
+                command = new SqlCommand(queryString, cnn);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    this.lstBook.Items.Add(
+                        "Id: " + reader["Id"].ToString() + "  " +
+                        "Name: " + reader["Name"].ToString() + "  " +
+                        "Author: " + reader["Author"].ToString() + "  " +
+                        "Published Date: " + reader["PublishDate"].ToString().Substring(0, 10) + "  " +
+                        "ISBN: " + reader["ISBN"].ToString());
+                }
+            }
+
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error in SQL! " + ex.Message);
+            }
+
+            finally
+            {
+                if (cnn.State == ConnectionState.Open)
+                {
+                    cnn.Close();
+                }
+            }
+        }
+
+        // event handler for edit button
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cnn.Open();
+                command = new SqlCommand("update dbo.BOOKS set Name = @bookname, Author = @author," +
+                                         "PublishDate = @publishdate, ISBN = @ISBN " +
+                                         "where id = @id", cnn);
+
+                string str = this.lstBook.SelectedItem.ToString();
+                string bookid = str.Substring(4, str.IndexOf(" ") - 1);
+
+                command.Parameters.AddWithValue("@bookname", this.txtName.Text);
+                command.Parameters.AddWithValue("@author", this.txtAuthor.Text);
+                command.Parameters.AddWithValue("@publishdate", this.txtDate.Text);
+                command.Parameters.AddWithValue("@ISBN", this.txtISBN.Text);
+                command.Parameters.AddWithValue("@id", bookid);
+
+                int r = command.ExecuteNonQuery();
+
+                MessageBox.Show(r + " Book record updated");
+
+                cnn.Close();
+            }
+
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error in SQL! " + ex.Message);
+            }
+
+            finally
+            {
+                if (cnn.State == ConnectionState.Open)
+                {
+                    cnn.Close();
+                }
+            }
+        }
+
+        // event handler for delete button
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cnn.Open();
+                command = new SqlCommand("delete from dbo.BOOKS where id = @id", cnn);
+
+                string str = this.lstBook.SelectedItem.ToString();
+                string bookid = str.Substring(4, str.IndexOf(" ") -1);
+
+                command.Parameters.AddWithValue("@id", bookid);
+
+                int r = command.ExecuteNonQuery();
+
+                MessageBox.Show(r + " Book record deleted");
+
+                cnn.Close();
+            }
+
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error in SQL! " + ex.Message);
+            }
+
+            finally
+            {
+                if (cnn.State == ConnectionState.Open)
+                {
+                    cnn.Close();
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
