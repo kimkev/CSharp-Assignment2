1b5e6c5ab6eec6384de4ac99abe137bc5445dda5
kimkev <43860707+kimkev@users.noreply.github.com>
2021-01-20 14:03:18 -0500

    Add files via upload

---
+++ Assignment2c/Assignment2c/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Assignment2c
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBookId = new System.Windows.Forms.TextBox();
+            this.txtBookName = new System.Windows.Forms.TextBox();
+            this.txtReviewerName = new System.Windows.Forms.TextBox();
+            this.lstBooks = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BookID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Book Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Reviewer Name";
+            // 
+            // txtBookId
+            // 
+            this.txtBookId.Location = new System.Drawing.Point(159, 49);
+            this.txtBookId.Name = "txtBookId";
+            this.txtBookId.Size = new System.Drawing.Size(114, 20);
+            this.txtBookId.TabIndex = 4;
+            // 
+            // txtBookName
+            // 
+            this.txtBookName.Location = new System.Drawing.Point(159, 104);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(114, 20);
+            this.txtBookName.TabIndex = 5;
+            // 
+            // txtReviewerName
+            // 
+            this.txtReviewerName.Location = new System.Drawing.Point(159, 154);
+            this.txtReviewerName.Name = "txtReviewerName";
+            this.txtReviewerName.Size = new System.Drawing.Size(114, 20);
+            this.txtReviewerName.TabIndex = 6;
+            // 
+            // lstBooks
+            // 
+            this.lstBooks.FormattingEnabled = true;
+            this.lstBooks.Location = new System.Drawing.Point(326, 49);
+            this.lstBooks.Name = "lstBooks";
+            this.lstBooks.Size = new System.Drawing.Size(376, 264);
+            this.lstBooks.TabIndex = 8;
+            this.lstBooks.SelectedIndexChanged += new System.EventHandler(this.lstBooks_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(323, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Matching Books";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(61, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Search Book By:";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(95, 249);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(152, 77);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Show Search!";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Location = new System.Drawing.Point(60, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 32);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Clear Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 344);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lstBooks);
+            this.Controls.Add(this.txtReviewerName);
+            this.Controls.Add(this.txtBookName);
+            this.Controls.Add(this.txtBookId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBookId;
+        private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.TextBox txtReviewerName;
+        private System.Windows.Forms.ListBox lstBooks;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Assignment2c/Assignment2c/Form1.cs
@@ -0,0 +1,108 @@
+﻿/* Assignment 2 Task 3: Book search form
+ * Kevin Kim 
+ * 991 456 570
+ * June 13, 2018
+ */
+using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Assignment2c
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = null;
+        SqlConnection cnn;
+        SqlCommand command;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Book Search";
+
+            connectionString = "Data Source=DESKTOP-4P2PB1M\\SQLEXPRESS;" +
+                              "Initial Catalog = MegaBookDB;" +
+                              "Integrated Security = SSPI; " +
+                              "Persist Security Info = false";
+
+            cnn = new SqlConnection(connectionString);
+        }
+
+        // event handler for search button
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // empty out the 
+                this.lstBooks.Items.Clear();
+
+                cnn.Open();
+                
+                // search condition using provided input
+                if (String.IsNullOrEmpty(this.txtBookId.Text) && String.IsNullOrEmpty(this.txtBookName.Text) &&
+                    String.IsNullOrEmpty(this.txtReviewerName.Text))
+                {
+                    // alert for empty input
+                    MessageBox.Show("Enter at least one search criteria");
+                }
+                else
+                {
+                    
+                    command = new SqlCommand("SELECT * FROM dbo.BOOKS, dbo.REVIEWS " +
+                                            "WHERE dbo.books.id = dbo.reviews.bookid " +
+                                            "AND dbo.books.id = @bookid " +
+                                            "OR dbo.books.name = @bookname " +
+                                            "OR dbo.reviews.reviewer = @reviewername", cnn);
+
+                    command.Parameters.AddWithValue("@bookid", this.txtBookId.Text);
+                    command.Parameters.AddWithValue("@bookname", this.txtBookName.Text);
+                    command.Parameters.AddWithValue("@reviewername", this.txtReviewerName.Text);
+
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            this.lstBooks.Items.Add(
+                                "BookID: " + reader["BookID"].ToString() + "  " +
+                                "Book: " + reader["Name"].ToString() + "  " +
+                                "Reviewer: " + reader["Reviewer"].ToString() + "  " +
+                                "Rating: " + reader["Rating"].ToString() + Environment.NewLine + Environment.NewLine);
+                        }
+                    }
+                
+            }
+
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error in SQL! " + ex.Message);
+            }
+
+            finally
+            {
+                if (cnn.State == ConnectionState.Open)
+                {
+                    cnn.Close();
+                }
+            }
+        }
+
+        // event handler for clear button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.txtBookId.Clear();
+            this.txtBookName.Clear();
+            this.txtReviewerName.Clear();
+        }
+
+        private void lstBooks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
